81543d9f5eac6176f813b3e31c90d146fc76f360
Nicholas Merolle <nmerolle11@hotmail.com>
2020-09-04 01:19:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-  array.join(" and ")
+  n = 1
+  while n < array.length
+    if n == 1
+      array.join
+      n += 1
+    else if n == 2
+      array.join(" and ")
+      n += 1
+    else
+      arr = []
+      
 end
